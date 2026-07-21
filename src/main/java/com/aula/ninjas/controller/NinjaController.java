55ec7949ad6c9e78e1536f03a0781a37dbcb3077
Andrius Farias <andriusfarias02@gmail.com>
2026-07-20 22:29:29 -0300

    #1 Criando a Controller

--- src/main/java/com/aula/ninjas/controller/NinjaController.java
+++ src/main/java/com/aula/ninjas/controller/NinjaController.java
@@ -14,15 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
  * STATEFULL -> ESTADO É MANTIDO, USUÁRIO JÁ FICA LOGADO, E TOKEN DE AUTENTICAÇÃO JÁ FICA ATIVO E APENAS É RENOVADO PARA ENVIO DA REQUISIÇÃO.
  * */
 @RestController //anotação para indicar que a classe é um controlador e REST, para retornar resposta HTTP com Json, misturando @Controller e @RequestBody
-@RequestMapping("/")
+@RequestMapping("/ninjas-v1")
 public class NinjaController {
 
     @Autowired
     private NinjaService NinjaService;
 
-    @GetMapping
-    public String getNome(){
+    @GetMapping("/nome")
+    public String index(){
         return NinjaService.index("teste");
+    }
+
+    @GetMapping("/teste")
+    public boolean teste(){
+        return NinjaService.teste(2);
     }
 
 
